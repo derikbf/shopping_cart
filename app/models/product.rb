15c14375bbc9a1941634cd7a5b75e8b33e84fd82
derik.favoretti <derikb@gmail.com>
2025-09-26 21:05:43 -0300

    removido comentários esquecidos rs

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-  has_many :cart_items # um produto pode estar em vários itens de carrinho
-  has_many :carts, through: :cart_items # um produto pode estar em vários carrinhos
+  has_many :cart_items
+  has_many :carts, through: :cart_items
 
   validates_presence_of :name, :price
   validates_numericality_of :price, greater_than_or_equal_to: 0
