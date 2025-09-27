800271bccb59e35cfafc466826e4ea1eaf9ad613
derik.favoretti <derikb@gmail.com>
2025-09-27 13:53:43 -0300

    add regra para o job e teste

--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -5,5 +5,10 @@ class MarkCartAsAbandonedJob
 
   def perform(*args)
     # TODO Impletemente um Job para gerenciar, marcar como abandonado. E remover carrinhos sem interação. 
+    abandon_threshold = 3.hours.ago
+    Cart.where(status: 'active').where('last_interaction_at < ?', abandon_threshold).update_all(status: 'abandoned')
+
+    delete_threshold = 7.days.ago
+    Cart.where(status: 'abandoned').where('last_interaction_at < ?', delete_threshold).destroy_all
   end
 end
--- spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
+++ spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
@@ -1,3 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+
 RSpec.describe MarkCartAsAbandonedJob, type: :job do
+  include ActiveJob::TestHelper
+
+  it "marks inactive carts as 'abandoned'" do
+    create(:cart, status: 'active', last_interaction_at: 4.hours.ago)
+    recent_cart = create(:cart, status: 'active', last_interaction_at: 1.hour.ago)
+
+    perform_enqueued_jobs { described_class.perform_later }
+
+    expect(Cart.first.status).to eq('abandoned')
+    expect(recent_cart.reload.status).to eq('active')
+  end
+
+  it "deletes carts that have been abandoned for more than 7 days" do
+    create(:cart, status: 'abandoned', last_interaction_at: 8.days.ago)
+    create(:cart, status: 'abandoned', last_interaction_at: 1.day.ago)
+
+    expect {
+      perform_enqueued_jobs { described_class.perform_later }
+    }.to change(Cart, :count).by(-1)
+  end
 end
