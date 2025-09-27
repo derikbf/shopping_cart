91d9983106f2d6c4ac124ea124dcab15cf74b12f
derik.favoretti <derikb@gmail.com>
2025-09-26 21:04:40 -0300

    Criado modelo de ligacao entre Produto e Carrinho - CartItem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  has_many :cart_items # um produto pode estar em vários itens de carrinho
+  has_many :carts, through: :cart_items # um produto pode estar em vários carrinhos
+
   validates_presence_of :name, :price
   validates_numericality_of :price, greater_than_or_equal_to: 0
 end
