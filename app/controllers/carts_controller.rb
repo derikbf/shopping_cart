b96035e04b99072168ebe3fdb1b4bc1d818f4feb
derik.favoretti <derikb@gmail.com>
2025-09-27 11:29:45 -0300

    Primeiro teste parcial criado, e concern de carrinho na sessao

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,4 +2,20 @@
 
 class CartsController < ApplicationController
   ## TODO Escreva a lógica dos carrinhos aqui
+
+  include CurrentCart
+  before_action :set_cart
+
+  def add_product
+    product = Product.find(params[:product_id])
+    
+    cart_item = @cart.cart_items.find_or_initialize_by(product_id: product.id)
+    cart_item.quantity = (cart_item.quantity || 0) + params[:quantity].to_i
+    cart_item.save!
+
+    render json: {
+      id: @cart.id,
+      products: [{ id: product.id, quantity: cart_item.quantity }]
+    }, status: :created
+  end
 end
---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module CurrentCart
+  extend ActiveSupport::Concern
+
+  private
+
+  def set_cart
+    @cart = Cart.find_by(id: session[:cart_id])
+
+    if @cart.nil?
+      @cart = Cart.create!(total_price: 0)
+      session[:cart_id] = @cart.id
+    end
+  end
+end
